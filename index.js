76c6bba281d307e58c3734e61efb19d5817ec3a3
Fran Herrero <francisco.herrero@axa-groupsolutions.com>
2022-03-14 14:31:15 +0100

    feat: add sonarqube reporter type

--- index.js
+++ index.js
@@ -1,86 +1,85 @@
 #!/usr/bin/env node
 
-module.exports = Formatter
+module.exports = Formatter;
 
-var util = require('util')
-var reporters = require('./lib/reporters/index.js')
-Formatter.types = Object.keys(reporters).sort()
-var Writable = require('stream').Writable
+var util = require('util');
+var reporters = require('./lib/reporters/index.js');
+Formatter.types = Object.keys(reporters).sort();
+var Writable = require('stream').Writable;
 
-var Runner = require('./lib/runner.js')
-var Parser = require('tap-parser')
+var Runner = require('./lib/runner.js');
+var Parser = require('tap-parser');
 
-util.inherits(Formatter, Writable)
+util.inherits(Formatter, Writable);
 
-var exitCode
-function Formatter (type, options) {
+var exitCode;
+function Formatter(type, options) {
   if (!(this instanceof Formatter)) {
-    return new Formatter(type, options)
+    return new Formatter(type, options);
   }
   if (!reporters[type]) {
-    console.error('Unknown format type: %s\n\n%s', type, avail())
-    type = 'silent'
+    const avail = avail();
+    console.error('Unknown format type: %s\n\n%s', type, avail);
+    throw new Error('Unknown format type: %s\n\n%s', type, avail);
   }
 
-  this.writable = true
+  this.writable = true;
 
   // don't actually need a reporter to report the tap we're getting
   // just parse it so that we exit with the correct code, but otherwise
   // dump it straight through to stdout.
   if (type === 'tap') {
-    var p = new Parser()
+    var p = new Parser();
     this.write = function (chunk) {
-      process.stdout.write(chunk)
-      return p.write(chunk)
-    }
-    this.end = p.end.bind(p)
+      process.stdout.write(chunk);
+      return p.write(chunk);
+    };
+    this.end = p.end.bind(p);
     p.on('complete', function () {
-      if (!p.ok)
-        exitCode = 1
-    })
-    return this
+      if (!p.ok) exitCode = 1;
+    });
+    return this;
   }
 
-  var runner = this.runner = new Runner(options)
-  this.reporter = new reporters[type](this.runner, options)
-  Writable.call(this, options)
+  var runner = (this.runner = new Runner(options));
+  this.reporter = new reporters[type](this.runner, options);
+  Writable.call(this, options);
 
   runner.on('end', function () {
-    if (!runner.parser.ok)
-      exitCode = 1
-  })
+    if (!runner.parser.ok) exitCode = 1;
+  });
 }
 
 process.on('exit', function (code) {
-  if (!code && exitCode)
-    process.exit(exitCode)
-})
+  if (!code && exitCode) process.exit(exitCode);
+});
 
 Formatter.prototype.write = function () {
-  return this.runner.write.apply(this.runner, arguments)
-}
+  return this.runner.write.apply(this.runner, arguments);
+};
 
 Formatter.prototype.end = function () {
-  return this.runner.end.apply(this.runner, arguments)
+  return this.runner.end.apply(this.runner, arguments);
+};
+
+function avail() {
+  var types = Formatter.types
+    .reduce(function (str, t) {
+      var ll = str.split('\n').pop().length + t.length;
+      if (ll < 40) return str + ' ' + t;
+      else return str + '\n' + t;
+    }, '')
+    .trim();
+
+  return 'Available format types:\n\n' + types;
 }
 
-function avail () {
-  var types = Formatter.types.reduce(function (str, t) {
-    var ll = str.split('\n').pop().length + t.length
-    if (ll < 40)
-      return str + ' ' + t
-    else
-      return str + '\n' + t
-  }, '').trim()
-
-  return 'Available format types:\n\n' + types
-}
-
-
-function usage (err) {
-  console[err ? 'error' : 'log'](function () {/*
+function usage(err) {
+  console[err ? 'error' : 'log'](
+    function () {
+      /*
 Usage:
-  tap-mocha-reporter <type>
+  tap-reporter <type>
 Options:
 	-o <file>,  --output <file> If present, writes report to given path, otherwise prints to stdout.
 	-p <prependPath>, --prependTestFileName <prependPath> If present, prepends to the file path the value of the flag
@@ -88,11 +87,17 @@ Reads TAP data on stdin, and formats to stdout using the specified
 reporter.  (Note that some reporters write to files instead of stdout.)
 
 %s
-*/}.toString().split('\n').slice(1, -1).join('\n'), avail())
+*/
+    }
+      .toString()
+      .split('\n')
+      .slice(1, -1)
+      .join('\n'),
+    avail()
+  );
 }
 
 if (require.main === module) {
-
   function getOutputPath(flag) {
     const index = process.argv.indexOf(flag);
     if (process.argv[index + 1]) {
@@ -110,7 +115,7 @@ if (require.main === module) {
     return '';
   }
 
-  let outputPath ='';
+  let outputPath = '';
   let prependTestFileName = '';
   if (process.argv.indexOf('--output') > 0) {
     outputPath = getOutputPath('--output');
@@ -120,15 +125,18 @@ if (require.main === module) {
   }
 
   if (process.argv.indexOf('-p') > 0) {
-   prependTestFileName = getPrependTestFileName('-p');
+    prependTestFileName = getPrependTestFileName('-p');
   }
 
   if (process.argv.indexOf('--prependTestFileName') > 0) {
     prependTestFileName = getPrependTestFileName('--prependTestFileName');
   }
 
-  var type = process.argv[2]
-  if (!type)
-    return usage()
-  process.stdin.pipe(new Formatter(type, {reporterOptions: {output: outputPath, prependTestFileName}}))
+  var type = process.argv[2];
+  if (!type) return usage();
+  process.stdin.pipe(
+    new Formatter(type, {
+      reporterOptions: { output: outputPath, prependTestFileName },
+    })
+  );
 }
